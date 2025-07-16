57749a935c3dd23644eee0f5a1ed683cd6ceb151
abdulkayumbauphal <kayum12123@gmail.com>
2025-07-16 22:23:41 +0600

    uppercase-to-lower-case-letter-without-library.c

---
+++ uppercase-to-lower-case-letter-without-library.c
@@ -0,0 +1,15 @@
+//uppercase-to-lower-case-letter-without-library
+#include<stdio.h>
+
+int main(){
+
+    char upper;
+
+    printf("Enter Any Uppercase Letter : ");
+    scanf("%c", &upper); // suppose usr entered a that ascii value is 65
+
+    printf("The Uppercase Letter :%c", upper+32); //a ascii value is a, we use minus 97-65=32 mane 32 er diffarence a to A eta sob captial and small letter er jonno hobe
+    // upper=32=lowercase latter oi same letter er
+
+
+}
